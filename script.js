152f1b05fee158b8121bb659109a9c1aaeaa105e
Guido Asbun <guido@asbun.io>
2021-06-23 15:32:42 -0700

    added images

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+// let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+
+let images = ['fireworks1.png', 'fireworks2.png', 'fireworks3.png', 'fireworks4.png']
+
+function preload() {
+  img = loadImage('fireworks2.png');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+    background('#383838');
+
 }
 
 function draw() {
   noStroke();
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  // fill(random(colorlist));
+  // ellipse(mouseX, mouseY, 25, 25);
+  if (frameCount % 20 == 0) {
+    let size = random(50, 300)
+    // let currImg = images[frameCount % images.length]
+    image(img, mouseX, mouseY, size, size)
+  }
+  
 }
+
